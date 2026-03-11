2af566196ba382f4dfd055cf83c61c8560743a22
diogonogueira2025 <diogovictor2203@gmail.com>
2026-03-11 14:23:47 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,12 @@
 module workshop_javafx_jdbc {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+    
+    // Se o seu JDBC for usado mais tarde, você precisará disso:
+    // requires java.sql; 
+
+    opens application to javafx.graphics, javafx.fxml;
+    
+    // ESTA É A LINHA QUE FALTA:
+    opens gui to javafx.fxml;
 }
